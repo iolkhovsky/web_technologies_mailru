f82f526cd0d977e39f740f9d132157bf103dd801
Igor <i.v.olkhovsky@gmail.com>
2020-10-23 16:11:15 +0300

    Settings modified

--- django_projects/qa_site/ask/ask/settings.py
+++ django_projects/qa_site/ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u$jjzii8yq=wjm1(l&gzh1#6tj!1tb=%53-@)&fd)grbo(f7%i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
